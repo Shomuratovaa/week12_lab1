2432b4215e39a2e480ea5ac39a96c33d4e6a596b
Shomuratovaa <sarvinozshomuratova4@gmail.com>
2025-11-30 09:42:57 +0500

    Refactor array and pointer usage with random values
    Revised pointer and array initialization, added random value assignment.

--- problem3.cpp
+++ problem3.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main() {
-    int *ptrA;
-    int arr[10]={1,2,3,4,5,6,7,8,9};
-    ptrA=arr;
-
+    srand(time(0));
+    int arr[10];
+    int *ptrA=arr;
     for (int i=0; i<10; i++) {
-        cout<<"Adress: "<<ptrA<<" Value: "<<*ptrA<<endl;
-        ptrA++;
+        arr[i]=rand()%100;
     }
-
+    for (int i=0; i<10; i++) {
+        cout<<"Address: "<<(ptrA+i)<<endl;
+        cout<<"Value: "<<*ptrA+i<<endl;
+    }
     return 0;
 }
